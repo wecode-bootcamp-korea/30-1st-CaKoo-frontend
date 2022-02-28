109a75d86101e2fa706a9d33f8c3a523e4414ce7
cos <junzerokim@gmail.com>
2022-02-28 17:50:04 +0900

    Modify: variables.scss import remove

--- src/index.js
+++ src/index.js
@@ -5,6 +5,5 @@ import Router from './Router';
 
 import './styles/reset.scss';
 import './styles/common.scss';
-import './styles/variables.scss';
 
 ReactDOM.render(<Router />, document.getElementById('root'));
