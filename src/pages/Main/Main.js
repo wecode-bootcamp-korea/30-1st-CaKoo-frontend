18b96042fbd251e26b34dc65e51603bcbc618089
전슬기 <jeonseulgi@jeonseulgiui-MacBook-Air.local>
2022-02-28 21:35:55 +0900

    Add : Main(상품 리스트) 페이지 뼈대 및 scss

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,5 +1,28 @@
+import { Link } from 'react-router-dom';
+
+import './Main.scss';
+
 function Main() {
-  return <div />;
+  return (
+    <>
+      <nav className="Nav">
+        <Link to="/">CaKoo 로고</Link>
+        <div>
+          <Link to="/login">로그인</Link>
+          <Link to="/sign-up">회원가입</Link>
+          <Link to="/cart">장바구니</Link>
+        </div>
+      </nav>
+      <main className="Main">
+        <div className="banner">
+          <p>케이쿠</p>
+          <p>감성을 담은 케이크</p>
+          <p>나를 위한 나만의 케이크</p>
+        </div>
+      </main>
+      <footer className="Footer" />
+    </>
+  );
 }
 
 export default Main;
