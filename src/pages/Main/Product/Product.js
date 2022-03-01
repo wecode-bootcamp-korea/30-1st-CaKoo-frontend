8d25a5976e6eedca9581b41c14c939be2931b049
전슬기 <jeonseulgi@jeonseulgiui-MacBook-Air.local>
2022-03-01 16:38:42 +0900

    Modify: Product 컴포넌트 수정

--- src/pages/Main/Product/Product.js
+++ src/pages/Main/Product/Product.js
@@ -1,14 +1,19 @@
+import { Link } from 'react-router-dom';
 import './Product.scss';
 
 function Product() {
   return (
     <article className="Product">
-      <img
-        alt="상품 미리보기(샘플)"
-        className="productImage"
-        src="https://github.com/Pbang91/cakooimage/blob/main/images/macaroncake.jpeg?raw=true"
-      />
-      <h4 className="productName">마카롱 무화과 케이크</h4>
+      <Link to="/product-detail">
+        <img
+          alt="상품 미리보기(샘플)"
+          className="productImage"
+          src="https://github.com/Pbang91/cakooimage/blob/main/images/macaroncake.jpeg?raw=true"
+        />
+      </Link>
+      <Link to="/product-detail" className="productName">
+        마카롱 무화과 케이크
+      </Link>
       <p className="priceInfo">
         <span className="discountRate">10%</span>
         <span className="productPrice">32,000원</span>
