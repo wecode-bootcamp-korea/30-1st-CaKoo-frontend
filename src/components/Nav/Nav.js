7799d6a35a29a72cbc4b8e9ce380d6f23d8df01d
전슬기 <jeonseulgi@jeonseulgiui-MacBook-Air.local>
2022-03-02 16:13:18 +0900

    Add: Nav, Footer 컴포넌트 & common.scss 수정

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,5 +1,29 @@
+import { Link } from 'react-router-dom';
+import './Nav.scss';
+
 function Nav() {
-  return <div />;
+  return (
+    <nav className="Nav">
+      <Link to="/">
+        <img
+          alt="cakoo 로고"
+          className="logo"
+          src="img/sophie/cakoo_logo.png"
+        />
+      </Link>
+      <ul className="navUser">
+        <li>
+          <Link to="/login">로그인</Link>
+        </li>
+        <li>
+          <Link to="/sign-up">회원가입</Link>
+        </li>
+        <li>
+          <Link to="/cart">장바구니</Link>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
 export default Nav;
