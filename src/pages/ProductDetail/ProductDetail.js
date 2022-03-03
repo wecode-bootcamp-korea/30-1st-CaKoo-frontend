cad782aebf5bcb97bd6466df9cc0301a434bb2c4
전슬기 <jeonseulgi@jeonseulgiui-MacBook-Air.local>
2022-03-03 20:49:09 +0900

    Modify : 상품 상세 페이지 데이터 fetch 준비중

--- src/pages/ProductDetail/ProductDetail.js
+++ src/pages/ProductDetail/ProductDetail.js
@@ -1,22 +1,36 @@
+import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import SmallImage from './SmallImage/SmallImage';
 
 import './ProductDetail.scss';
 
 function ProductDetail() {
-  const fakeData = {
-    id: 1,
-    name: '딸기 쿠키 케이크',
-    price: 32000,
-    description: '특별한 날에 어울리는,',
-    image: '/images/strawberry_biscuit_cake.jpeg',
-  };
+  const [product, setProduct] = useState({});
+
+  useEffect(() => {
+    fetch('http://10.58.3.222:8000/product?name=', {
+      method: 'GET',
+    })
+      .then(res => res.json())
+      .then(result => {
+        console.log(result);
+        setProduct(result);
+      });
+  }, []);
+
+  //   const fakeData = {
+  //     id: 1,
+  //     name: '딸기 쿠키 케이크',
+  //     price: 32000,
+  //     description: '특별한 날에 어울리는,',
+  //     image: '/images/strawberry_biscuit_cake.jpeg',
+  //   };
 
   return (
     <main className="productDetail">
       <div className="category">
         <Link to="/">Home</Link>
-        <span>{` > 케이크 > ${fakeData.name}`}</span>
+        <span>{` > 케이크 > ${product.name}`}</span>
       </div>
 
       <div className="wrapper">
@@ -24,20 +38,20 @@ function ProductDetail() {
           <img
             alt="상품 이미지 크게 보기"
             className="mainImage"
-            src={fakeData.image}
+            src={product.image}
           />
           <div className="smallImages">
-            <SmallImage data={fakeData} />
-            <SmallImage data={fakeData} />
-            <SmallImage data={fakeData} />
-            <SmallImage data={fakeData} />
-            <SmallImage data={fakeData} />
+            <SmallImage data={product} />
+            <SmallImage data={product} />
+            <SmallImage data={product} />
+            <SmallImage data={product} />
+            <SmallImage data={product} />
           </div>
         </div>
 
         <div className="order">
-          <h1>{fakeData.name}</h1>
-          <h3>{`${fakeData.price}원`}</h3>
+          <h1>{product.name}</h1>
+          <h3>{`${product.price}원`}</h3>
           <hr />
           <hr />
         </div>
