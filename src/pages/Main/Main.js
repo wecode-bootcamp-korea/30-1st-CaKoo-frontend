4cbfc596f54d0bd3ca80e4c06718740f79a9c8fc
전슬기 <jeonseulgi@jeonseulgiui-MacBook-Air.local>
2022-03-04 20:06:37 +0900

    Modify : Product 컴포넌트 props  구조분해 할당(리뷰 반영)

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,61 +1,47 @@
 import { useState, useEffect } from 'react';
-
 import Banner from './Banner/Banner';
 import Product from './Product/Product';
-
 import './Main.scss';
 
 function Main() {
   const [productList, setProductList] = useState([]);
 
   useEffect(() => {
-    fetch('http://10.58.3.222:8000/products/list', {
-      method: 'GET',
-    })
+    fetch('http://10.58.5.164:8000/products')
       .then(res => res.json())
       .then(result => {
-        console.log(result.lists);
+        // console.log(result.lists);
         setProductList(result.lists);
       });
   }, []);
 
-  // const fakeData = [
+  // const productList = [
   //   {
   //     id: 1,
   //     name: '딸기 쿠키 케이크',
   //     description: '특별한 날에 어울리는,',
-  //     discount_rate: 10,
+  //     discount_rate: 0.1,
   //     price: 32000,
   //     sizes: ['1호', '2호'],
-  //     thumnail: '',
+  //     thumnail: '/images/strawberry_biscuit_cake.jpeg',
   //   },
   //   {
   //     id: 2,
   //     name: '딸기 쿠키 케이크',
-  //     price: 32000,
   //     description: '특별한 날에 어울리는,',
+  //     discount_rate: 0.1,
+  //     price: 32000,
+  //     sizes: ['1호', '2호'],
+  //     thumnail: '/images/strawberry_biscuit_cake.jpeg',
   //   },
   // ];
 
-  if (!productList.length) {
-    return null;
-  }
   return (
     <main className="main">
       <Banner />
       <div className="productContainer">
-        <h1>{productList[0].name}</h1>
         {productList.map(data => (
-          <Product
-            key={data.id}
-            id={data.id}
-            name={data.name}
-            price={data.price}
-            rate={data.discount_rate}
-            description={data.description}
-            sizes={data.sizes}
-            url={data.thumbnail}
-          />
+          <Product key={data.id} data={data} />
         ))}
       </div>
       <button className="moreProduct">더보기</button>
--- src/pages/Main/Product/Product.js
+++ src/pages/Main/Product/Product.js
@@ -1,24 +1,34 @@
 import { Link } from 'react-router-dom';
 import './Product.scss';
 
-function Product({ id, name, price, rate, description, sizes, url }) {
+function Product({ data }) {
+  const { id, name, price, discount_rate, description, sizes, thumbnail } =
+    data;
   return (
     <article className="product">
       <Link to={`/product-detail/${id}`}>
-        <img alt="상품 미리보기(샘플)" className="productImage" src={url} />
+        <img
+          alt="상품 미리보기(샘플)"
+          className="productImage"
+          src={thumbnail}
+        />
       </Link>
       <span className="productInfo">{description}</span>
       <Link to={`/product-detail/${id}`} className="productName">
         {name}
       </Link>
       <p className="priceInfo">
-        <span className="discountRate">{`${parseInt((1 - rate) * 100)}%`}</span>
+        <span className="discountRate">{`${parseInt(
+          (1 - discount_rate) * 100
+        )}%`}</span>
         <span className="productPrice">{`${parseInt(price)}원`}</span>
-        <span className="discountedPrice">{`${price * rate}원`}</span>
+        <span className="discountedPrice">{`${price * discount_rate}원`}</span>
       </p>
       <p className="otherInfo">
-        {/* {sizes.map(size => (
-          <span className="productSize">{size}</span>
+        {/* {sizes.map((size, index) => (
+          <span key={index} className="productSize">
+            {size}
+          </span>
         ))} */}
         <span className="productSize">1호</span>
         <span className="productSize">2호</span>
