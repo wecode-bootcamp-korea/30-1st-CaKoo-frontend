e1738f503fa391b65bfaed42b935578732c683a1
agnesshin <y.agnes.shin@gmail.com>
2022-03-04 13:33:28 +0900

    Modify: Login Page 수정  완료

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,79 +1,94 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Login.scss';
-
-// const onChangeBtn = () => {
-//   const [idValue, setIdValue] = useState('');
-//   const [pwValue, setPwValue] = useState('');
-
-//   const updateIdValue = e => {
-//     setIdValue(e);
-//   };
-//   setPwValue(e.target.value);
-
-//   const condition = idValue.length > 0 && pwValue.length > 0;
-// };
-
-// function LogIn() {
-//   const [isLogInOn, setLogInOn] = useState(false);
-//   const [color, setColor] = useState('#');
-//   const navigate = useNavigate();
-
-//   const goToMain = () => {
-//     setLogInOn(!setLogInOn);
-//   };
-
-//   const onChangeBtn = () => {
-//     setColor('#f9cc33;');
-//   };
-// }
-//   fetch ("https://www."); {
-//   method: "POST";
-//   body: JSON.stringy ({
-//     email: email,
-//     password: password
-//   }),
-// };
-//   .then((response)) => .json())
-//   .then((result) => console.log("결과:", result));
+import { useNavigate } from 'react-router-dom';
 
 function Login() {
+  const [idValue, setIdValue] = useState('');
+  const [pwValue, setPwValue] = useState('');
+  // const navigate = useNavigate();
+
+  const handleIdInput = e => {
+    setIdValue(e.target.value);
+  };
+
+  const handlePwInput = e => {
+    setPwValue(e.target.value);
+    console.log(idValue, pwValue);
+  };
+
+  const isValid = () => {
+    if (idValue.includes('@') && pwValue.length >= 8) {
+      return true;
+    }
+    return false;
+  };
+
+  // const condition = idValue.length > 0 && pwValue.length > 0;
+
+  const navigate = useNavigate();
+
+  const handleFetch = () => {
+    fetch('http://10.58.6.174:8000/users/signin', {
+      method: 'POST',
+      body: JSON.stringify({
+        email: 'test12@test.com',
+        password: 'test123!',
+      }),
+    })
+      .then(response => response.json())
+      .then(result => {
+        if (result.token) {
+          navigate('/main');
+        } else {
+          alert('가입된 회원이 아닙니다. 회원가입을 먼저 해주세요.');
+        }
+      });
+  };
+
   return (
     <div className="login">
       <h1>로그인</h1>
-      <div class="logInFormBox">
+      <div className="logInFormBox">
         <form className="logInForm">
           <input
             className="logInInput"
             type="text"
-            // onChange={upDateIdValue}
+            onChange={handleIdInput}
             placeholder="아이디 (이메일)"
             name="userName"
           />
           <input
             required
-            maxlength="15"
             className="logInInput"
             type="text"
-            // onChange={upDateIdValue}
+            onChange={handlePwInput}
             placeholder="비밀번호"
             text="password"
           />
           <div className="idSave">
             <input id="checkbox" className="checkBox" type="checked" />
-            <label for="checkbox" className="circle" />
+            <label htmlFor="checkbox" className="circle" />
             <span>아이디 저장</span>
           </div>
-          <button className="logInBtn">로그인</button>
+          <button
+            type="button"
+            className="logInBtn"
+            onClick={handleFetch}
+            disabled={!isValid()}
+            style={
+              isValid()
+                ? { backgroundColor: '#f9cc33' }
+                : { backgroundColor: '#ececec' }
+            }
+          >
+            로그인
+          </button>
         </form>
-      </div>
-      <div className="forGotContainer">
-        <Link to="/" className="forgot" alt="아이디 찾기">
-          아이디찾기
-        </Link>
-        <Link to="/" className="forgot" alt="비밀번호 찾기">
-          비밀번호 찾기
-        </Link>
+        <div className="forgotContainer">
+          <Link to="/" className="forgot" alt="아이디 찾기" /> 아이디찾기
+          <Link to="/" className="forgot" alt="비밀번호 찾기" /> 비밀번호 찾기
+        </div>
       </div>
     </div>
   );
