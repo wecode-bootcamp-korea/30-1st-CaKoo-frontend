67b54ea8b9f3b33611aa5e8efd10315de830f63f
agnesshin <y.agnes.shin@gmail.com>
2022-03-04 17:53:04 +0900

    Modify: Login.js,.css

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import './Login.scss';
-import { useNavigate } from 'react-router-dom';
 
 function Login() {
   const [idValue, setIdValue] = useState('');
