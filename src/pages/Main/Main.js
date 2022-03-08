730ff480238aa345aa49690a9a73c015ce140528
전슬기 <jeonseulgi@jeonseulgiui-MacBook-Air.local>
2022-03-08 21:53:13 +0900

    Modify : size 필터 기능 되살림

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -6,45 +6,46 @@ import './Main.scss';
 function Main() {
   const [productList, setProductList] = useState([]);
   const [sort, setSort] = useState('');
-  // const [filterSize, setFilterSize] = useState([]);
+  const [filterSize, setFilterSize] = useState([]);
 
   const URI = 'http://10.58.1.160:8000/products';
 
-  useEffect(() => {
-    fetch(sort ? `${URI}?sort=${sort}` : URI)
-      .then(res => res.json())
-      .then(result => {
-        // console.log(result.lists);
-        setProductList(result.lists);
-      });
-  }, [sort]);
-
-  // size filter 있을 때
+  // size 필터 없을 때
   // useEffect(() => {
-  //   fetch(
-  //     sort
-  //       ? filterSize.length
-  //         ? `${URI}?sort=${sort}&size=${filterSize.join()}`
-  //         : `${URI}?sort=${sort}`
-  //       : filterSize.length
-  //       ? `${URI}?size=${filterSize.join()}`
-  //       : URI
-  //   )
+  //   fetch(sort ? `${URI}?sort=${sort}` : URI)
   //     .then(res => res.json())
   //     .then(result => {
-  //       console.log(result.lists);
+  //       // console.log(result.lists);
   //       setProductList(result.lists);
   //     });
-  // }, [sort, filterSize]);
+  // }, [sort]);
 
-  // function handleCheck(event) {
-  //   const size = event.target.name;
-  //   if (filterSize.includes(size)) {
-  //     setFilterSize(filterSize.filter(element => element !== size));
-  //   } else {
-  //     setFilterSize([...filterSize, size]);
-  //   }
-  // }
+  // size filter 있을 때
+  useEffect(() => {
+    fetch(
+      sort
+        ? filterSize.length
+          ? `${URI}?sort=${sort}&size=${filterSize.join()}`
+          : `${URI}?sort=${sort}`
+        : filterSize.length
+        ? `${URI}?size=${filterSize.join()}`
+        : URI
+    )
+      .then(res => res.json())
+      .then(result => {
+        console.log(result.lists);
+        setProductList(result.lists);
+      });
+  }, [sort, filterSize]);
+
+  function handleCheck(event) {
+    const size = event.target.name;
+    if (filterSize.includes(size)) {
+      setFilterSize(filterSize.filter(element => element !== size));
+    } else {
+      setFilterSize([...filterSize, size]);
+    }
+  }
 
   // const productList = [
   //   {
@@ -71,7 +72,7 @@ function Main() {
     <main className="main">
       <Banner />
       <div className="filterBar">
-        {/* form에 method 사용?? filter 사용 안 하는 중.
+        {/* form에 method 사용?? filter 사용 안 하는 중. */}
         <form className="filterForm">
           <label>
             <input type="checkbox" name="1" onChange={handleCheck} />
@@ -89,7 +90,7 @@ function Main() {
             <input type="checkbox" name="4" onChange={handleCheck} />
             3호
           </label>
-        </form> */}
+        </form>
         <div>
           <button
             type="button"
