6e133d009cee3088fcbb2d90d3e6359f1609f882
cos <junzerokim@gmail.com>
2022-03-09 14:39:52 +0900

    Add: Delete Fetch

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -7,43 +7,50 @@ import './Cart.scss';
 
 const Cart = () => {
   const [cartProduct, setCartProduct] = useState([]);
-
-  // useEffect(() => {
-  //   fetch('http://localhost:3000/src/pages/Cart/cart.json')
-  //     .then(res => res.json())
-  //     .then(result => setCartProduct(result));
-  // }, []);
-
-  const mockData = [
-    {
-      cart_id: 1,
-      product_name: '딸기 비스킷 케이크',
-      product_size: 'mini',
-      product_price: 49800,
-      cart_quantity: 3,
-      thumbnail: '/images/strawberry_biscuit_cake.jpeg',
-    },
-    {
-      cart_id: 2,
-      product_name: '블루베리 비스킷 케이크',
-      product_size: '1호',
-      product_price: 59800,
-      cart_quantity: 3,
-      thumbnail: '/images/strawberry_biscuit_cake.jpeg',
-    },
-    {
-      cart_id: 3,
-      product_name: '바닐라 비스킷 케이크',
-      product_size: '2호',
-      product_price: 79800,
-      cart_quantity: 3,
-      thumbnail: '/images/strawberry_biscuit_cake.jpeg',
-    },
-  ];
+  const token = localStorage.getItem('Authorization');
 
   useEffect(() => {
-    setCartProduct(mockData);
+    fetch('http://10.58.6.143:8000/carts', {
+      headers: {
+        Authorization: token,
+      },
+    })
+      .then(res => res.json())
+      .then(result => {
+        setCartProduct(result.message);
+      });
   }, []);
+
+  // const mockData = [
+  //   {
+  //     cart_id: 1,
+  //     product_name: '딸기 비스킷 케이크',
+  //     product_size: 'mini',
+  //     product_price: 49800,
+  //     cart_quantity: 3,
+  //     thumbnail: '/images/strawberry_biscuit_cake.jpeg',
+  //   },
+  //   {
+  //     cart_id: 2,
+  //     product_name: '블루베리 비스킷 케이크',
+  //     product_size: '1호',
+  //     product_price: 59800,
+  //     cart_quantity: 3,
+  //     thumbnail: '/images/strawberry_biscuit_cake.jpeg',
+  //   },
+  //   {
+  //     cart_id: 3,
+  //     product_name: '바닐라 비스킷 케이크',
+  //     product_size: '2호',
+  //     product_price: 79800,
+  //     cart_quantity: 3,
+  //     thumbnail: '/images/strawberry_biscuit_cake.jpeg',
+  //   },
+  // ];
+
+  // useEffect(() => {
+  //   setCartProduct(mockData);
+  // }, []);
 
   const totalPrice = () => {
     let price = 0;
@@ -54,21 +61,18 @@ const Cart = () => {
     return price;
   };
 
-  // const buyProduct = () => {
-  //   fetch('')
-  //     .then(res => res.json())
-  //     .then(result => setCartProduct(result));
-  // };
-
   const deleteCart = id => {
     setCartProduct(cartProduct =>
       cartProduct.filter(product => product.cart_id !== id)
     );
-    fetch('http://123.456:8000/delete', {
-      method: 'POST',
-      body: { cart_id: id },
+    fetch(`http://10.58.6.143:8000/carts/${id}`, {
+      method: 'DELETE',
+      headers: {
+        Authorization: token,
+      },
     });
   };
+
   return (
     <main className="cart">
       <header className="cartHeader">
@@ -88,11 +92,9 @@ const Cart = () => {
                 );
               })}
               <BuyConfirm totalPrice={totalPrice().toLocaleString('ko-KR')} />
-              <Button
-                className="button"
-                value="구매하기"
-                // onClick={buyProduct}
-              />
+              <Link to="/order">
+                <Button className="button" value="구매하기" />
+              </Link>
             </>
           ) : (
             <>
